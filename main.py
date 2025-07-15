9b464e8fddfdca1b339e2f8c06b65643a24db6d7
Equillibru <133333214+Equillibru@users.noreply.github.com>
2025-07-15 18:04:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ def fetch_prices():
 
 # Stocks (Yahoo Finance)
    for t in SP500_TICKERS:
-        try:
-        hist = yf.Ticker(t).history(period="1h", interval="5m")['Close'].dropna().tolist()
-        data[t] = hist[-6:] if len(hist) >= 6 else hist
+    try:
+       hist = yf.Ticker(t).history(period="1h", interval="5m")['Close'].dropna().tolist()
+       data[t] = hist[-6:] if len(hist) >= 6 else hist
     except Exception as e:
         print(f"⚠️ Error fetching stock {t}: {e}")
 
